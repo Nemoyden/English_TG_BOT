81c460ec059ca7816d570127c8beb7623fde2fb6
Nemoyden <robert.kharazov@gmail.com>
2025-01-03 19:49:21 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,5 @@
+"""
+Пакет word_quiz_bot предназначен для запуска Telegram-бота, помогающего пользователям запоминать английские слова через квизы.
+"""
+
+__all__ = ['bot', 'exceptions', 'decorators', 'data_handler', 'scheduler', 'utils']
