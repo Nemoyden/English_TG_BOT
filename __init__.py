416105dad33544bcc238704a183832525bea0c8b
Nemoyden <robert.kharazov@gmail.com>
2025-01-03 19:52:16 +0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Пакет word_quiz_bot предназначен для запуска Telegram-бота, помогающего пользователям запоминать английские слова через квизы.
-"""
-
-__all__ = ['bot', 'exceptions', 'decorators', 'data_handler', 'scheduler', 'utils']
